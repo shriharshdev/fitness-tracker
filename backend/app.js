77fb7b268554ba8df962051fb4ad1e518523d513
Shriharsh <shriharsh.dev@gmail.com>
2023-12-30 09:34:06 +0530

    Added database connection

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+mongoose.set("strictQuery", false)
+const mongoDB = "mongodb+srv://shriharshdev:qFn8qxlFRvRL87Ef@cluster0.dwcgoij.mongodb.net/fitnessTracker?retryWrites=true&w=majority"
+
+main().catch((err) => console.log(err))
+async function main(){
+  await mongoose.connect(mongoDB)
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
