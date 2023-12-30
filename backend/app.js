c89a9882f675a42be856742661b47844992a1909
Shriharsh <shriharsh.dev@gmail.com>
2023-12-30 23:09:31 +0530

    Modified routes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const registerRouter = require("./routes/register");
 
 const app = express();
@@ -32,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use("/gym",registerRouter)
 
 // catch 404 and forward to error handler
