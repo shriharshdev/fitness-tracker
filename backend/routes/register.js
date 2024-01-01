42dbe8d240c25927db1c950ca6baeb4e436495be
Shriharsh <shriharsh.dev@gmail.com>
2024-01-01 12:35:43 +0530

    Added routes for displaying users

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -9,4 +9,7 @@ router.post("/exercise",exercise.exercise_form_post)
 router.get("/user",user.user_form_get)
 router.post("/user",user.user_form_post)
 
+router.get("/users",user.user_list)
+router.get("/user/:id",user.user_details)
+
 module.exports = router
